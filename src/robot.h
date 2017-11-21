25cd53a25e54b15a4395bfc8400d5cbb3bd0edb3
Alena Kazakova <alenadanse@gmail.com>
2017-11-21 21:39:56 +0900

    Update robot.h

--- src/robot.h
+++ src/robot.h
@@ -1,9 +1,6 @@
 #ifndef __ROBOT_H__
 #define __ROBOT_H__
-#include <signal.h>
-#include <stdio.h>
-#include <wiringPi.h>
-#include <sys/time.h>
+
 
 /* WiringPi pin numbering scheme */
 /* Ultrasonic sensor pins */
@@ -27,153 +24,20 @@
 #define LEFT		'L'
 #define STOP		'S'
 
-/* Function declarations */
-void setup	(void);
-void sigHandler (int sigNo);
-void ultraInit	(void);
-float getRange	(void);
-void motorInit  (void);
-void setSpeed   (int speedA, int speedB);
-void setDir     (char dir);
-void goFW       (void);
-void goBW       (void);
-void turnR      (void);
-void turnL      (void);
-void stop       (void);
+extern void setup	(void);
 
-void setup(void)
-{
-	/* Ctrl-C handler */
-	if (signal(SIGINT, sigHandler) == SIG_ERR){
-		perror("Error: cannot handle SIGINT\n");
-	}
-	
-	if (wiringPiSetup() == -1) {
-		exit(1);
-	}
+extern void sigHandler (int sigNo);
 
-	ultraInit();
-	motorInit();
-}
+extern void ultraInit	(void);
+extern float getRange	(void);
 
-void sigHandler(int sigNo)
-{
-	printf("Caught SIGINT, exiting now\n");
-	setDir(STOP);
-	setSpeed(0, 0);
-	exit(0);
-}
-
-void ultraInit(void)
-{
-	pinMode(Echo, INPUT);
-	pinMode(Trig, OUTPUT);
-}
-
-float getRange(void)
-{
-	struct timeval tv1;
-	struct timeval tv2;
-	long start, stop;
-	float range;
-
-	digitalWrite(Trig, LOW);
-	delayMicroseconds(2);
-
-	digitalWrite(Trig, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(Trig, LOW);
-
-	while (!(digitalRead(Echo) == 1));
-	gettimeofday(&tv1, NULL);
-
-	while (!(digitalRead(Echo) == 0));
-	gettimeofday(&tv2, NULL);
-
-	start = tv1.tv_sec * 1000000 + tv1.tv_usec;
-	stop  = tv2.tv_sec * 1000000 + tv2.tv_usec;
-
-	range = (float)(stop - start) / 1000000 * 34000 / 2;
-	//range = (stop - start) * 34000 / 1000000 / 2;
-
-	return range;
-}
-
-void motorInit(void) 
-{
-	pinMode(MotorIn1, OUTPUT);
-	pinMode(MotorIn2, OUTPUT);
-	pinMode(MotorIn3, OUTPUT);
-	pinMode(MotorIn4, OUTPUT);
-	pinMode(MotorEnA, PWM_OUTPUT);
-	pinMode(MotorEnB, PWM_OUTPUT);
-}
-
-void setSpeed(int speedA, int speedB) 
-{
-	pwmWrite(MotorEnA, speedA);
-	pwmWrite(MotorEnB, speedB);
-}
-
-void setDir(char dir) 
-{
-	switch (dir) {
-		case 'F':
-			goFW();
-			break;
-		case 'B':
-			goBW();
-			break;
-		case 'R':
-			turnR();
-			break;
-		case 'L':
-			turnL();
-			break;
-		case 'S':
-			stop();
-			break;
-	}
-}
-
-void goFW(void) 
-{
-	digitalWrite(MotorIn1, HIGH);
-	digitalWrite(MotorIn2, LOW);
-	digitalWrite(MotorIn3, HIGH);
-	digitalWrite(MotorIn4, LOW);
-}
-
-void goBW(void) 
-{
-	digitalWrite(MotorIn1, LOW);
-	digitalWrite(MotorIn2, HIGH);
-	digitalWrite(MotorIn3, LOW);
-	digitalWrite(MotorIn4, HIGH);
-}
-
-void turnR(void) 
-{
-	digitalWrite(MotorIn1, HIGH);
-	digitalWrite(MotorIn2, LOW);
-	digitalWrite(MotorIn3, LOW);
-	digitalWrite(MotorIn4, HIGH);
-}
-
-void turnL(void) 
-{
-	digitalWrite(MotorIn1, LOW);
-	digitalWrite(MotorIn2, HIGH);
-	digitalWrite(MotorIn3, HIGH);
-	digitalWrite(MotorIn4, LOW);
-}
-
-void stop(void) 
-{
-	digitalWrite(MotorIn1, LOW);
-	digitalWrite(MotorIn2, LOW);
-	digitalWrite(MotorIn3, LOW);
-	digitalWrite(MotorIn4, LOW);
-}
+extern void motorInit  (void);
+extern void setSpeed   (int speedA, int speedB);
+extern void setDir     (char dir);
+extern void goFW       (void);
+extern void goBW       (void);
+extern void turnR      (void);
+extern void turnL      (void);
+extern void stop       (void);
 
 #endif
