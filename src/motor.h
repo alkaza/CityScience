fa1025c010efb5215a993dcbe54d501f887e69e5
Alena Kazakova <alenadanse@gmail.com>
2017-11-21 15:18:17 +0900

    Create motor.h

---
+++ src/motor.h
@@ -0,0 +1,113 @@
+#ifndef __MOTOR_H__
+#define __MOTOR_H__
+#include <stdio.h>
+#include <wiringPi.h>
+
+/* WiringPi pin numbering scheme */
+
+/* Motor driver pins */
+/* Motor A */
+#define MotorEnA	1
+#define MotorIn1	2
+#define MotorIn2	3
+/* Motor B */
+#define MotorEnB	23
+#define MotorIn3	22
+#define MotorIn4	21
+
+/* Directions */
+#define FORWARD		'F'
+#define BACKWARD	'B'
+#define RIGHT		  'R'
+#define LEFT		  'L'
+#define STOP		  'S'
+
+
+/* Function declarations */
+void motorInit  (void);
+void setSpeed   (int speedA, int speedB);
+void setDir     (char dir);
+void goFW       (void);
+void goBW       (void);
+void turnR      (void);
+void turnL      (void);
+void stop       (void);
+
+void motorInit(void) 
+{
+	pinMode(MotorIn1, OUTPUT);
+	pinMode(MotorIn2, OUTPUT);
+	pinMode(MotorIn3, OUTPUT);
+	pinMode(MotorIn4, OUTPUT);
+	pinMode(MotorEnA, PWM_OUTPUT);
+	pinMode(MotorEnB, PWM_OUTPUT);
+}
+
+void setSpeed(int speedA, int speedB) 
+{
+	pwmWrite(MotorEnA, speedA);
+	pwmWrite(MotorEnB, speedB);
+}
+
+void setDir(char dir) 
+{
+	switch (dir) {
+		case 'F':
+			goFW();
+			break;
+		case 'B':
+			goBW();
+			break;
+		case 'R':
+			turnR();
+			break;
+		case 'L':
+			turnL();
+			break;
+		case 'S':
+			stop();
+			break;
+	}
+}
+
+void goFW(void) 
+{
+	digitalWrite(MotorIn1, HIGH);
+	digitalWrite(MotorIn2, LOW);
+	digitalWrite(MotorIn3, HIGH);
+	digitalWrite(MotorIn4, LOW);
+}
+
+void goBW(void) 
+{
+	digitalWrite(MotorIn1, LOW);
+	digitalWrite(MotorIn2, HIGH);
+	digitalWrite(MotorIn3, LOW);
+	digitalWrite(MotorIn4, HIGH);
+}
+
+void turnR(void) 
+{
+	digitalWrite(MotorIn1, HIGH);
+	digitalWrite(MotorIn2, LOW);
+	digitalWrite(MotorIn3, LOW);
+	digitalWrite(MotorIn4, HIGH);
+}
+
+void turnL(void) 
+{
+	digitalWrite(MotorIn1, LOW);
+	digitalWrite(MotorIn2, HIGH);
+	digitalWrite(MotorIn3, HIGH);
+	digitalWrite(MotorIn4, LOW);
+}
+
+void stop(void) 
+{
+	digitalWrite(MotorIn1, LOW);
+	digitalWrite(MotorIn2, LOW);
+	digitalWrite(MotorIn3, LOW);
+	digitalWrite(MotorIn4, LOW);
+}
+
+#endif
