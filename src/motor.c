1220f8b98276897a4e765a111f8de63b3e0faf4d
Alena Kazakova <alenadanse@gmail.com>
2017-11-21 15:26:09 +0900

    Update motor.c

--- src/motor.c
+++ src/motor.c
@@ -1,165 +1,34 @@
-#include <stdio.h>
-#include <signal.h>
-#include <wiringPi.h>
-
-/* WiringPi pin numbering scheme */
-
-/* Motor driver pins */
-/* Motor A */
-#define MotorEnA	1
-#define MotorIn1	2
-#define MotorIn2	3
-/* Motor B */
-#define MotorEnB	23
-#define MotorIn3	22
-#define MotorIn4	21
-
-/* Directions */
-#define FORWARD		'F'
-#define BACKWARD	'B'
-#define RIGHT		'R'
-#define LEFT		'L'
-#define STOP		'S'
+#include "motor.h"
 
 /* Speed */
 #define SPEED		200 	// possible range 0~255 PWM
 
-/* Function declarations */
-void sigHandler (int sigNo);
-void motorInit  (void);
-void move	(char dir, int speedA, int speedB);
-void setSpeed   (int speedA, int speedB);
-void goFW       (void);
-void goBW       (void);
-void turnR      (void);
-void turnL      (void);
-void stop       (void);
-
 int main(void)
 {
-	/* Ctrl-C handler */
-	if (signal(SIGINT, sigHandler) == SIG_ERR){
-		perror("Error: cannot handle SIGINT\n");
-	}
+	setup();
 	
-	/* Setup code, runs once */
-	if (wiringPiSetup() == -1) {
-		exit(1);
-	}
-
-	motorInit();
-
-	/* Main code, runs repeatedly */
+	setSpeed(SPEED, SPEED);
+	
 	while (1) {
 		/* Go straight */
-		move(FORWARD, SPEED, SPEED);
+		setDir(FORWARD);
 
 		/* Go back */
-		//move(BACKWARD, SPEED, SPEED);
+		//setDir(BACKWARD);
 		//delay(150);
 		
 		/* Turn right */
-		//move(RIGHT, SPEED, SPEED);
+		//setDir(RIGHT);
 		//delay(250);
 		
 		/* Turn left */
-		//move(LEFT, SPEED, SPEED);
+		//setDir(LEFT);
 		//delay(250);
 		
 		/* Stop */
-		//move(STOP, SPEED, SPEED);
-		
+		//setDir(STOP);
 		//delay(3000);
 	}
 
 	return 0;
-}
-
-void sigHandler(int sigNo)
-{
-	printf("Caught SIGINT, exiting now\n");
-	move(STOP, 0, 0);
-	exit(0);
-}
-
-void motorInit(void) 
-{
-	pinMode(MotorIn1, OUTPUT);
-	pinMode(MotorIn2, OUTPUT);
-	pinMode(MotorIn3, OUTPUT);
-	pinMode(MotorIn4, OUTPUT);
-	pinMode(MotorEnA, PWM_OUTPUT);
-	pinMode(MotorEnB, PWM_OUTPUT);
-}
-
-void move(char dir, int speedA, int speedB) 
-{
-	switch (dir) {
-		case 'F':
-			goFW();
-			setSpeed(speedA, speedB);
-			break;
-		case 'B':
-			goBW();
-			setSpeed(speedA, speedB);
-			break;
-		case 'R':
-			turnR();
-			setSpeed(speedA, speedB);
-			break;
-		case 'L':
-			turnL();
-			setSpeed(speedA, speedB);
-			break;
-		case 'S':
-			stop();
-			setSpeed(speedA, speedB);
-			break;
-	}
-}
-
-void setSpeed(int speedA, int speedB) 
-{
-	pwmWrite (MotorEnA, speedA);
-	pwmWrite (MotorEnB, speedB);
-}
-
-void goFW(void) 
-{
-	digitalWrite(MotorIn1, HIGH);
-	digitalWrite(MotorIn2, LOW);
-	digitalWrite(MotorIn3, HIGH);
-	digitalWrite(MotorIn4, LOW);
-}
-
-void goBW(void) 
-{
-	digitalWrite(MotorIn1, LOW);
-	digitalWrite(MotorIn2, HIGH);
-	digitalWrite(MotorIn3, LOW);
-	digitalWrite(MotorIn4, HIGH);
-}
-
-void turnR(void) 
-{
-	digitalWrite(MotorIn1, HIGH);
-	digitalWrite(MotorIn2, LOW);
-	digitalWrite(MotorIn3, LOW);
-	digitalWrite(MotorIn4, HIGH);
-}
-
-void turnL(void) 
-{
-	digitalWrite(MotorIn1, LOW);
-	digitalWrite(MotorIn2, HIGH);
-	digitalWrite(MotorIn3, HIGH);
-	digitalWrite(MotorIn4, LOW);
-}
-
-void stop(void) 
-{
-	digitalWrite(MotorIn1, LOW);
-	digitalWrite(MotorIn2, LOW);
-	digitalWrite(MotorIn3, LOW);
-	digitalWrite(MotorIn4, LOW);
 }
