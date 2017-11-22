5d3ee8eaa11791dc1dff9f959a59700f6d39b848
Alena Kazakova <alenadanse@gmail.com>
2017-11-22 14:24:36 +0900

    Update avoid.c

--- src/avoid.c
+++ src/avoid.c
@@ -10,8 +10,11 @@ int main(void)
 {
 	setup();
 	float dist;
+	int turning = 0;
+	int straight = 0;
+	int speed = SPEED;
+
 	setSpeed(SPEED, SPEED);
-	
 	while (1) {
 		dist = getDist();
 		printf("dist = %0.2f cm\n", dist);
@@ -19,12 +22,36 @@ int main(void)
 		
 		/* Modify here */
 		if ((dist < MINDIST) && (dist > 0)) {
-			printf("turn right\n");
-			setDir(RIGHT);
+			if (turning && (speed > 100) {
+				speed--;
+				setSpeed(speed, speed);
+			}
+			else {
+				straight = 0;
+				turning = 1;
+				speed = SPEED;
+				setSpeed(0, 0);
+				delay(300);
+				printf("turn right\n");
+				setDir(RIGHT);
+				setSpeed(speed, speed);
+			}
 		}
 		else {
-			printf("go straight\n");
-			setDir(FORWARD);
+			if (straight && (speed > 100)) {
+				speed--;
+				setSpeed(speed, speed);
+			}
+			else {	
+				turning = 0;
+				straight = 1;
+				speed = SPEED;
+				setSpeed(0, 0);
+				delay(300);
+				printf("go straight\n");
+				setDir(FORWARD);
+				setSpeed(speed, speed);
+			}
 		}
 	}
 
