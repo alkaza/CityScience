15186a392f1adea206b00dc434d6b6b0b8f89322
Alena Kazakova <alenadanse@gmail.com>
2017-11-23 00:29:26 +0900

    Update robot.h

--- src/lib/robot.h
+++ src/lib/robot.h
@@ -2,42 +2,57 @@
 #define __ROBOT_H__
 
 /* WiringPi pin numbering scheme */
-
 /* Ultrasonic sensor pins */
 #define Trig	4
 #define Echo	5
-
 /* Motor driver pins */
 /* Motor A */
 #define MotorEnA	1
 #define MotorIn1	2
 #define MotorIn2	3
 /* Motor B */
+#define MotorIn3	21
+#define MotorIn4	22
 #define MotorEnB	23
-#define MotorIn3	22
-#define MotorIn4	21
 
 /* Direction modes */
 #define FW  'F'
 #define BW  'B'
 #define RIGHT 'R'
 #define LEFT  'L'
-#define STOP  'S'
 
-extern void setup	(void);
+/* Flags */
+extern volatile int turningR = 0;
+extern volatile int turningL = 0;
+extern volatile int goingFW = 0;
+extern volatile int goingBW = 0;
 
-extern void sigHandler (int sigNo);
+/* Track current speed */
+extern volatile int speed;
 
-extern void ultraInit	(void);
-extern float getDist	(void);
+/* Setup function */
+extern void setup       (void);
+/* Set speed range */
+extern int max_speed    (int maxSpeed);
+extern int min_speed    (int minSpeed);
+/* Motor control functions */
+extern void move_slow   (char dir);
+/* Calulate distance */
+extern float calc_dist  (void);
 
-extern void motorInit  (void);
-extern void setSpeed   (int speedA, int speedB);
-extern void setDir     (char dir);
-extern void goFW       (void);
-extern void goBW       (void);
-extern void turnR      (void);
-extern void turnL      (void);
-extern void stop       (void);
+/* Initialization functions */
+extern void ultraInit   (void);
+extern void motorInit   (void);
+/* Motor helper functions */
+extern void setSpeed    (int speedA, int speedB);
+extern void setDir      (char dir);
+extern void goFW        (void);
+extern void goBW        (void);
+extern void turnR       (void);
+extern void turnL       (void);
+extern void brake       (void);
+extern void stop        (void);
+/* Signal handler */
+extern void sigHandler  (int sigNo);
 
 #endif
