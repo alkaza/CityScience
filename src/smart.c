f99abd11729c1c530d18006768a706d5da7f23d6
Alena Kazakova <alenadanse@gmail.com>
2017-11-24 19:11:28 +0900

    Update smart.c

--- src/smart.c
+++ src/smart.c
@@ -4,8 +4,8 @@
 
 /* Macros */
 #define MINDIST		10	// possible range 2~400 cm (3.3V)
-#define SPEEDA		120 	// possible range 60~255 PWM
-#define SPEEDB		130
+#define SPEEDA		150 	// possible range 60~255 PWM
+#define SPEEDB		155
 
 int flag = 0;
 
@@ -20,16 +20,16 @@ int main(void)
 		/* Modify here */
 		if ((dist < MINDIST) && (dist > 0)) {
 			if (flag) {
-				move_slow(LEFT, SPEEDA, SPEEDB);
+				move(LEFT, SPEEDA, SPEEDB);
 				flag = 0;
 			}
 			else {
-				move_slow(RIGHT, SPEEDA, SPEEDB);
+				move(RIGHT, SPEEDA, SPEEDB);
 				flag = 1;
 			}
 		}
 		else {
-			move_slow(FW, SPEEDA, SPEEDB);
+			move(FW, SPEEDA, SPEEDB);
 		}
 	}
 
